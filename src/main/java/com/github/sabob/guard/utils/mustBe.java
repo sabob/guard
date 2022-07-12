aa88f917ca7461214986dfefd65ae73b201409ce
Bob Schellink <sabob1@gmail.com>
2022-07-12 12:23:48 +0200

    more utilities

--- src/main/java/com/github/sabob/guard/utils/mustBe.java
+++ src/main/java/com/github/sabob/guard/utils/mustBe.java
@@ -1,10 +1,20 @@
 package com.github.sabob.guard.utils;
 
+import com.github.sabob.guard.constraints.FalseOnly;
 import com.github.sabob.guard.constraints.NullOnly;
+import com.github.sabob.guard.constraints.TrueOnly;
 
 public class mustBe {
 
     public static NullOnly Null() {
         return new NullOnly();
+    }
+
+    public static TrueOnly True() {
+        return new TrueOnly();
+    }
+
+    public static FalseOnly False() {
+        return new FalseOnly();
     }
 }
